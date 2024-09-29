b82f4cb1ba5ea715916d3afd4cd39b498fd9e716
Hendriansyah <hendriansyah170522@gmail.com>
2024-09-29 08:34:35 +0700

    Fix

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -3,10 +3,13 @@ import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
 
 const cache = new InMemoryCache();
 
-await persistCache({
-  cache,
-  storage: new LocalStorageWrapper(window.localStorage),
-});
+async function setupCache() {
+  await persistCache({
+    cache,
+    storage: new LocalStorageWrapper(window.localStorage),
+  });
+}
+setupCache();
 
 const apolloClient = new ApolloClient({
   uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
